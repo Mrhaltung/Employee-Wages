ddae96b43466c947dc2ad028131eb8c2e4d00e72
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-02 12:54:52 +0530

    Add project files.

---
+++ Employee_Wages/Employee_Wages/Emp_Wages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wages
+{
+    class Emp_Wages
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Problem. ");
+            
+            Emp_Attendence.empCheck();
+            Emp_Daily_Wage.DailyEmpWage();
+            Part_Time_Emp_Wage.PartTimeEmpWage();
+            Case_Statement.CaseStatement();
+            Monthly_Emp_Wage.MonthlyEmpWage();
+            While_Loop.WhileLoop();
+            Method.Methods();
+
+            Console.ReadLine();
+        }
+    }
+}
